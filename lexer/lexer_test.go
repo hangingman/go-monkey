108f9568041f6195c86850e202717f12fc9fb917
hiroyuki.nagata <hiroyuki.nagata@freestylewiki.xyz>
2018-10-29 22:14:34 -0400

    Change format for correct Golang style

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,39 +1,52 @@
 package lexer
 
 import (
-    "testing"
-    "bitbucket.org/hangedman/go-monkey/token"
+	"testing"
+	"bitbucket.org/hangedman/go-monkey/token"
 )
 
+func TestStatements(t *testing.T) {
+	input := `let five = 5;
+let ten = 10;
+
+let add = fn(x, y) {
+  x + y;
+}
+
+let result = add(five, ten);
+`
+
+}
+
 func TestNextToken(t *testing.T) {
-    input := `=+(),;`
+	input := `=+(),;`
 
-    tests := []struct {
-        expectedType token.TokenType
-        expectedLiteral string
-    }{
-        {token.ASSIGN, "="},
-        {token.PLUS, "+"},
-        {token.LPAREN, "("},
-        {token.RPAREN, ")"},
-        {token.COMMA, ","},
-        {token.SEMICOLON, ";"},
-        {token.EOF, ""},
-    }
+	tests := []struct {
+		expectedType token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
 
-    l := New(input)
+	l := New(input)
 
-    for i, tt := range tests {
-        tok := l.NextToken()
+	for i, tt := range tests {
+		tok := l.NextToken()
 
-        if tok.Type != tt.expectedType {
-            t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
-            i, tt.expectedType, tok.Type)
-        }
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
 
-        if tok.Literal != tt.expectedLiteral {
-            t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
-            i, tt.expectedLiteral, tok.Literal)            
-        }
-    }
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)            
+		}
+	}
 }
