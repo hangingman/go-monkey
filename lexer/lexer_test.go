5dd8a0239685737dab6b87b1cbc4bfdbd6de2fa7
hiroyuki.nagata <hiroyuki.nagata@freestylewiki.xyz>
2018-10-30 21:16:05 -0400

    Add tokenized tokens

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -16,6 +16,39 @@ let add = fn(x, y) {
 let result = add(five, ten);
 `
 
+	tests := []struct {
+		expectedType token.TokenType
+		expectedLiteral string
+	}{
+		{token.LET,       "let"},
+		{token.IDENT,     "five"},
+		{token.ASSIGN,    "="},
+		{token.INT,       "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET,       "let"},
+		{token.IDENT,     "ten"},
+		{token.ASSIGN,    "="},
+		{token.INT,       "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET,       "let"},
+		{token.IDENT,     "add"},
+		{token.ASSIGN,    "="},
+		{token.FUNCTION,  "fn"},
+		{token.LPAREN,    "("},
+		{token.IDENT,     "x"},
+		{token.COMMA,     ","},
+		{token.IDENT,     "y"},
+		{token.RPAREN,    ")"},
+		{token.LBRACE,    "{"},
+		{token.IDENT,     "x"},
+		{token.PLUS,      "+"},
+		{token.IDENT,     "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE,    "}"},
+	}
+	l := New(input)
+
+
 }
 
 func TestNextToken(t *testing.T) {
@@ -46,7 +79,7 @@ func TestNextToken(t *testing.T) {
 
 		if tok.Literal != tt.expectedLiteral {
 			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
-				i, tt.expectedLiteral, tok.Literal)            
+				i, tt.expectedLiteral, tok.Literal)
 		}
 	}
 }
