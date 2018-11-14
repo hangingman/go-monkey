53058ced59e0f9b816f51edcf5a132965c0a0621
hiroyuki nagata <newserver002@gmail.com>
2018-11-14 23:14:01 +0900

    Add testcases, p.10

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -45,10 +45,33 @@ let result = add(five, ten);
 		{token.IDENT,     "y"},
 		{token.SEMICOLON, ";"},
 		{token.RBRACE,    "}"},
+        {token.LET,       "let"},
+        {token.IDENT,     "result"},
+        {token.ASSIGN,    "="},
+        {token.IDENT,     "add"},
+        {token.LPAREN,    "("},
+        {token.IDENT,     "five"},
+        {token.COMMA,     ","},
+        {token.IDENT,     "ten"},
+        {token.RPAREN,    ")"},
+        {token.SEMICOLON, ";"},
+        {token.EOF,       ""},
 	}
 	l := New(input)
 
+	for i, tt := range tests {
+		tok := l.NextToken()
 
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+	}
 }
 
 func TestNextToken(t *testing.T) {
