2f5fcdd5b6f80bc74241cea7b8dfb1c90570cfd9
hiroyuki nagata <newserver002@gmail.com>
2018-11-24 20:11:23 +0900

    Enable to read sentence as tokens, p.14

--- token/token.go
+++ token/token.go
@@ -1,7 +1,11 @@
 package token
 
+// TokenType は stringのエイリアス
 type TokenType string
 
+// Token は文字列をトークン化した後の情報を保持する
+// TokenType にはtoken内で定義したconst値
+// Literal には実際に読み取った文字列
 type Token struct {
 	Type TokenType
 	Literal string
@@ -23,3 +27,15 @@ const (
 	FUNCTION  = "FUNCTION"
 	LET       = "LET"
 )
+
+var keywords = map[string] TokenType{
+    "fn": FUNCTION,
+    "let": LET,
+}
+
+func LookupIndent(ident string) TokenType {
+    if tok, ok := keywords[ident]; ok {
+        return tok
+    }
+    return IDENT
+}
