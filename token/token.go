b26eaa65e08ccab6656e13606127cd68531b712e
hiroyuki nagata <newserver002@gmail.com>
2018-11-24 21:14:03 +0900

    Enable to read if/else/return/true/false, p.17

--- token/token.go
+++ token/token.go
@@ -32,11 +32,21 @@ const (
     GT        = ">"
 	FUNCTION  = "FUNCTION"
 	LET       = "LET"
+    TRUE      = "TRUE"
+    FALSE     = "FALSE"
+    IF        = "IF"
+    ELSE      = "ELSE"
+    RETURN    = "RETURN"
 )
 
 var keywords = map[string] TokenType{
     "fn": FUNCTION,
     "let": LET,
+    "true": TRUE,
+    "false": FALSE,
+    "if": IF,
+    "else": ELSE,
+    "return": RETURN,
 }
 
 func LookupIndent(ident string) TokenType {
