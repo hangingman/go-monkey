4d0b288b8a9fef7d802b561a9b6f7dafd5015b69
hiroyuki nagata <hiroyuki@cowboy.freestylewiki.xyz>
2018-11-27 08:25:10 +0900

    Add ast.go

---
+++ ast/ast.go
@@ -0,0 +1,19 @@
+package ast
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
