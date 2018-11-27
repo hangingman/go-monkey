6effa1cd5de4f7797f24c87af0ebe6c597649722
hiroyuki nagata <hiroyuki@cowboy.freestylewiki.xyz>
2018-11-28 07:54:45 +0900

    Add ast.go code, p.33

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,9 @@
 package ast
 
+
+import "bitbucket.org/hangedman/go-monkey/token"
+
+
 type Node interface {
 	TokenLiteral() string
 }
@@ -17,3 +21,28 @@ type Expression interface {
 type Program struct {
 	Statements []Statement
 }
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) statementNode() {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
