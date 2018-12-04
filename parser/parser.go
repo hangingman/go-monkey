dcad03667410d692b56c1be526b6d952a984bd95
hiroyuki.nagata <hiroyuki.nagata@freestylewiki.xyz>
2018-12-03 23:15:57 -0500

    Fix test cases

--- parser/parser.go
+++ parser/parser.go
@@ -17,10 +17,11 @@ func New(l *lexer.Lexer) *Parser {
 	// ２つトークンを読み込む。curTokenとpeekTokenの両方がセットされる。
 	p.nextToken()
 	p.nextToken()
+	return p
 }
 
 func (p *Parser) nextToken() {
-	p.curToken = p.peekToken()
+	p.curToken = p.peekToken
 	p.peekToken = p.l.NextToken()
 }
 
---
+++ parser/parser_test.go
@@ -0,0 +1,13 @@
+package parser
+
+import (
+	"testing"
+	// "github.com/hangingman/go-monkey/ast"
+	// "github.com/hangingman/go-monkey/lexer"
+)
+
+func TestLetStatements(t *testing.T) {
+}
+
+func TestLetStatement(t *testing.T) {
+}
