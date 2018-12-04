663993436e24586eade33b9b1b90f222d1d505ec
hiroyuki.nagata <hiroyuki.nagata@freestylewiki.xyz>
2018-12-03 23:03:30 -0500

    Write parser.go, p.35

--- parser/parser.go
+++ parser/parser.go
@@ -1,7 +1,29 @@
 package parser
 
-// import (
-//     "github.com/hangingman/go-monkey/ast"
-//     "github.com/hangingman/go-monkey/lexer"
-//     "github.com/hangingman/go-monkey/token"
-// )
+import (
+	"github.com/hangingman/go-monkey/ast"
+	"github.com/hangingman/go-monkey/lexer"
+	"github.com/hangingman/go-monkey/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+	curToken token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+	// ２つトークンを読み込む。curTokenとpeekTokenの両方がセットされる。
+	p.nextToken()
+	p.nextToken()
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken()
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
