cebcff57a7073f1bd5e54b1a437b60e27b56570c
hiroyuki nagata <newserver002@gmail.com>
2018-12-23 21:36:57 +0900

    Write test for simple statement

--- min/parser/parser_simple_statement_test.go
+++ min/parser/parser_simple_statement_test.go
@@ -1,14 +1,16 @@
 package parser
 
 import (
-	"fmt"
 	"github.com/hangingman/go-monkey/min/ast"
 	"github.com/hangingman/go-monkey/min/lexer"
+	"github.com/hangingman/go-monkey/min/token"    
 	"testing"
 )
 
 func TestSimpleStatements(t *testing.T) {
-	input := `input x`
+	input := `input x
+output y
+`
 
 	l := lexer.New(input)
 	p := New(l)
@@ -19,54 +21,48 @@ func TestSimpleStatements(t *testing.T) {
 		t.Fatalf("ParseProgram() returned nil")
 	}
 
-	for _, stmt := range program.Statements {
-		fmt.Printf("%s\n", stmt)
-	}
+	// for _, stmt := range program.Statements {
+	// 	fmt.Printf("%s\n", stmt)
+	// }
 
-	if len(program.Statements) != 1 {
-		t.Fatalf("program.Statements does not contain 1 statements. got=%d",
+	if len(program.Statements) != 2 {
+		t.Fatalf("program.Statements does not contain 2 statements. got=%d",
 			len(program.Statements))
 	}
 
 	tests := []struct {
+        expectedToken token.Token
 		expectedIdentifer string
 	}{
-		{"x"},
+		{token.Token{Type: token.INPUT, Literal: string("input")}, "x"},
+        {token.Token{Type: token.OUTPUT, Literal: string("output")}, "y"},
 	}
 
 	for i, tt := range tests {
 		stmt := program.Statements[i]
-		if !testSimpleStatement(t, stmt, tt.expectedIdentifer) {
+		if !testSimpleStatement(t, stmt, tt.expectedToken, tt.expectedIdentifer) {
 			return
 		}
 	}
 }
 
-func testSimpleStatement(t *testing.T, s ast.Statement, expectNames string) bool {
+func testSimpleStatement(t *testing.T, s ast.Statement, expectedToken token.Token, expectedName string) bool {
 
-	if s.TokenLiteral() != "input" {
-		t.Errorf("s.TokenLiteral not 'var'. got=%q", s.TokenLiteral())
+	simpleStmt, ok := s.(*ast.SimpleStatement)
+	if !ok {
+		t.Errorf("s not *ast.SimpleStatement. got=%T", s)
 		return false
 	}
 
-	// simpleStmt, ok := s.(*ast.SimpleStatement)
-	// if !ok {
-	// 	t.Errorf("s not *ast.SimpleStatement. got=%T", s)
-	// 	return false
-	// }
-
-	// if len(expectNames) != len(simpleStmt.Names) {
-	// 	// fmt.Printf("%s", simpleStmt.Names)
-	// 	t.Errorf("simpleStmt.Names size not '%d', got=%d", len(expectNames), len(simpleStmt.Names))
-	// 	return false
-	// }
-
-	// for i, expectName := range expectNames {
-	// 	if expectName != simpleStmt.Names[i].Value {
-	// 		t.Errorf("simpleStmt.Names[%d].Value not '%s', got=%s", i, expectName, simpleStmt.Names[i].Value)
-	// 		return false
-	// 	}
-	// }
+    if expectedToken != simpleStmt.Token {
+        t.Errorf("simpleStmt.Name.Token not '%s', got=%s", expectedToken, simpleStmt.Token)
+        return false
+    }
+    
+    if expectedName != simpleStmt.Name.Value {
+        t.Errorf("simpleStmt.Name.Value not '%s', got=%s", expectedName, simpleStmt.Name.Value)
+        return false
+    }
 
 	return true
 }
