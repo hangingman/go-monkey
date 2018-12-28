41a104b14ad270cd001990804870077956fd8053
hiroyuki.nagata <hiroyuki.nagata@hiroyuki-nagatas-MacBook-Pro.local>
2018-12-28 14:05:23 +0900

    Add complete test cases, p.75

--- parser/parser_expression_test.go
+++ parser/parser_expression_test.go
@@ -183,18 +183,66 @@ func TestOperatorPrecedenceParsing(t *testing.T) {
 			"-a * b",
 			"((-a) * b)",
 		},
+		{
+			"!-a",
+			"(!(-a))",
+		},
+		{
+			"a + b + c",
+			"((a + b) + c)",
+		},
+		{
+			"a + b - c",
+			"((a + b) - c)",
+		},
+		{
+			"a * b * c",
+			"((a * b) * c)",
+		},
+		{
+			"a / b / c",
+			"((a / b) / c)",
+		},
+		{
+			"a * b / c",
+			"((a * b) / c)",
+		},
+		{
+			"a + b / c",
+			"(a + (b / c))",
+		},
+		{
+			"a + b * c + d / e - f",
+			"(((a + (b * c)) + (d / e)) - f)",
+		},
+		{
+			"3 + 4; -5 * 5",
+			"(3 + 4)((-5) * 5)",
+		},
+		{
+			"5 > 4 == 3 < 4",
+			"((5 > 4) == (3 < 4))",
+		},
+		{
+			"5 > 4 != 3 > 4",
+			"((5 > 4) != (3 > 4))",
+		},
+		{
+			"3 + 4 * 5 == 3 * 1 + 4 * 5",
+			"((3 + (4 * 5)) == ((3 * 1) + (4 * 5)))",
+		},
+
 	}
 
 	for _, tt := range tests {
 		l := lexer.New(tt.input)
 		p := New(l)
 		program := p.ParseProgram()
-		// checkParserErrors(t, p)
+		checkParserErrors(t, p)
 		actual := program.String()
-		fmt.Printf("%v¥n", actual)
 
-		// if actual != tt.expected {
-		// 	t.Errorf("expected=%q, got=%q", tt.expected, actual)
-		// }
+		if actual != tt.expected {
+			t.Errorf("expected=%q, got=%q", tt.expected, actual)
+		}
 	}
 }
